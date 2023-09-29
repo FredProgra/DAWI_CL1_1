8a67bebeabb3038d79bcb9fff268c4eff5bf3cba
Fredo <alfredoqooo@gmail.com>
2023-09-29 16:52:29 -0500

    init comit

---
+++ src/test/java/edu/pe/cibertec/DAWI_CL1_1/DawiCl11ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.DAWI_CL1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
